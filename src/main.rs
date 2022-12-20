b1d8cc3ef053adeda6119342e2c1a336249cf309
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-12-20 14:58:44 +0500

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use screenshots::Screen;
+use std::{fs, time::Instant};
+
 fn main() {
-    println!("Hello, world!");
+    let start = Instant::now();
+    let screens = Screen::all().unwrap();
+
+    for screen in screens {
+        println!("capturer {:?}", screen);
+        let mut image = screen.capture().unwrap();
+        let mut buffer = image.buffer();
+        fs::write(format!("target/{}.png", screen.display_info.id), &buffer).unwrap();
+
+        image = screen.capture_area(300, 300, 300, 300).unwrap();
+        buffer = image.buffer();
+        fs::write(format!("target/{}-2.png", screen.display_info.id), &buffer).unwrap();
+    }
+
+    let screen = Screen::from_point(100, 100).unwrap();
+    println!("capturer {:?}", screen);
+
+    let image = screen.capture_area(300, 300, 300, 300).unwrap();
+    let buffer = image.buffer();
+    fs::write("target/capture_display_with_point.png", &buffer).unwrap();
+
+    println!("Time elapsed: {:?}", start.elapsed());
 }
